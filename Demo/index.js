17b0b3ea76e0b094b016a8ddd60e86442ea602b8
Chris <chris.j.thib@gmail.com>
2023-03-23 14:17:49 -0600

    blah

--- Demo/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require("path");
-let inputFile = path.join(__dirname, "input.txt")
-
-let contents = fs.readFileSync(inputFile).toString();
-
-console.log(contents);
-
